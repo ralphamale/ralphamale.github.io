efba59902c25fb9c93676982c8abe116da8fc189
Ralph Nguyen <ralph.nguyen@gmail.com>
2014-03-06 13:48:32 -0500

    Adds resume, lays framework for floatig pic.

---
+++ todo.js
@@ -0,0 +1,27 @@
+Make background like this for background: http://thenounproject.com/term/web-design/32232/
+or this: http://thenounproject.com/term/source-code/1171/ <-- theres a bit of them. the otehr source code one is cool.
+
+
+View Resume - http://thenounproject.com/term/eye/32668/
+Hide REsume - http://thenounproject.com/term/collapse/33657/
+Download http://thenounproject.com/term/download/10551/
+jeff king font
+http://siiiimple.eu.pn/
+http://www.jrk-design.co.uk/
+
+-Squeeze the resume in a little more so less empty space.
+
+-One more COntact Me photo.
+
+-Ask for Chamal input on color scheme
+
+-Bump font up for resume.
+
+Change font
+
+-Contact Form -- do i really need a whole rails app??
+
+-Picture of me will float to the "About Me" page
+
+
+-Add to linkedin, resume, etc.
